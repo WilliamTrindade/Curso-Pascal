7b611ad949d3e1e0ff91d0b711a9351d2307f16a
William Trindade <williamtrindade777@gmail.com>
2017-12-17 23:01:04 -0200

    Update case tipo3.pas

--- COMANDOS DE DECISAO/CASE/case tipo3.pas
+++ COMANDOS DE DECISAO/CASE/case tipo3.pas
@@ -7,28 +7,28 @@ begin
 	readln(Horas_trabalhadas);
 	writeln('Digite seu salario');
 	readln(salario);
-CASE Horas_trabalhadas OF
-0..30:
-begin
-			CASE salario OF
-				0..500:
-					begin
-						writeln('Voce ganha pouco mas nao se preocupe trabalhe duro e se esforce assim mesmo nao desista!');
-						readln;	
-					end;
-			end;		
-			CASE salario OF
-				501..1000:
-		   		begin
-		   			Writeln('Seu salario e razoavel é bom mesmo mas sempre queira mais!');
-		   			readln;
-		   		end;
-		   end
-end		   
-			else
-			 	begin
-					writeln('tu trabalha muito!');
-					readln; 
-				end;
-			end 							
+	CASE Horas_trabalhadas OF
+		0..30:
+			begin
+				CASE salario OF
+					0..500:
+						begin
+							writeln('Voce ganha pouco mas nao se preocupe trabalhe duro e se esforce assim mesmo nao desista!');
+							readln;	
+						end;
+				end;		
+				CASE salario OF
+					501..1000:
+					   	begin
+					   		Writeln('Seu salario e razoavel é bom mesmo mas sempre queira mais!');
+					   		readln;
+					   	end;
+				end
+	end		   
+	else
+		begin
+			writeln('tu trabalha muito!');
+			readln; 
+		end;
+	end 							
 end.
